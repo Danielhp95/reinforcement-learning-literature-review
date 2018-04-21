a97d8a612714c0c51e3c7b30590f3627831defd0
Daniel Hernandez <danielhp95@gmail.com>
2018-04-21 13:51:52 +0100

    Some modifications, of course

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 VAR=main
 rm -f $VAR.out $VAR.log $VAR.blg $VAR.bbl # Or however many of these you want
-pdflatex -shell-escape $VAR.tex
+pdflatex -synctex=1 -shell-escape $VAR.tex
 bibtex $VAR
 pdflatex -shell-escape $VAR.tex
 pdflatex -shell-escape $VAR.tex
